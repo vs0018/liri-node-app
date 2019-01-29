2661a76588dd92f2398a720f10d87d1615a4fb56
Valerie S <43964677+vs0018@users.noreply.github.com>
2019-01-29 17:37:21 -0500

    add switch case and move function

--- liri.js
+++ liri.js
@@ -9,16 +9,36 @@ var name = process.argv.slice(3).join("+");
 
 console.log(name);
 
-axios.get("http://www.omdbapi.com/?t=" + name + "&apikey=trilogy").then(
-  function(response) {
-    var movieInfo = response.data;
-
-    console.log("Title: " + movieInfo.Title);
-    console.log("Year: " + movieInfo.Year);
-    console.log("IMDb Rating: " + movieInfo.imdbRating);
-    console.log("Country: " + movieInfo.Country);
-    console.log("Language: " + movieInfo.Language);
-    console.log("Plot: " + movieInfo.Plot);
-    console.log("Actors: " + movieInfo.Actors);
+switch (command) {
+    case "concert-this":
+      total();
+      break;
+    
+    case "spotify-this-song":
+      deposit();
+      break;
+    
+    case "movie-this":
+      movieGet();
+      break;
+    
+    case "do-what-it-says":
+      lotto();
+      break;
     }
-);
+
+function movieGet() {
+    axios.get("http://www.omdbapi.com/?t=" + name + "&apikey=trilogy").then(
+    function(response) {
+        var movieInfo = response.data;
+
+        console.log("Title: " + movieInfo.Title);
+        console.log("Year: " + movieInfo.Year);
+        console.log("IMDb Rating: " + movieInfo.imdbRating);
+        console.log("Country: " + movieInfo.Country);
+        console.log("Language: " + movieInfo.Language);
+        console.log("Plot: " + movieInfo.Plot);
+        console.log("Actors: " + movieInfo.Actors);
+        }
+    );
+};
