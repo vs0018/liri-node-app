5033e7201de72f25460e47aeb6bcdfb997656809
Valerie S <43964677+vs0018@users.noreply.github.com>
2019-01-29 17:45:44 -0500

    fix Spotify require and variable

--- liri.js
+++ liri.js
@@ -2,7 +2,9 @@ require("dotenv").config();
 
 var keys = require("./keys.js");
 var axios = require("axios");
-// var spotify = new Spotify(keys.spotify);
+var Spotify = require('node-spotify-api');
+
+var spotify = new Spotify(keys.spotify);
 
 var command = process.argv[2];
 var name = process.argv.slice(3).join("+");
