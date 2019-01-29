77517607b21c0c527878b54b4d8faf7abf7a584d
Valerie S <43964677+vs0018@users.noreply.github.com>
2019-01-29 17:15:41 -0500

    create variables to take in user input and create axios call to omdb api

--- liri.js
+++ liri.js
@@ -1,5 +1,16 @@
 require("dotenv").config();
 
 var keys = require("./keys.js");
+var axios = require("axios");
+// var spotify = new Spotify(keys.spotify);
 
-var spotify = new Spotify(keys.spotify);
+var command = process.argv[2];
+var name = process.argv.slice(3).join("+");
+
+console.log(name);
+
+axios.get("http://www.omdbapi.com/?t=" + name + "&apikey=trilogy").then(
+  function(response) {
+    console.log(response);
+  }
+);
